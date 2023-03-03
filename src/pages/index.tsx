4600e81025456c5b4f657056ec994d41945ffa5f
Yewdev <Y_cooldev@outlook.com>
2023-03-04 04:21:47 +0700

    styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -25,18 +25,22 @@ const Home: NextPage = () => {
         </div>
       </main>
       {/* Coded by */}
-      <div className="group absolute right-0 bottom-0 flex items-end gap-2 rounded-tl-xl bg-white/5 py-3 pl-2 pr-10 text-sm tracking-widest text-white/70  duration-1000 hover:bg-black hover:text-white   hover:shadow-[0_0_30px_0px_#50ffa8]">
-        <span className="duration-500 group-hover:text-2xl">Challenge by</span>
+      <div className="group absolute right-0 bottom-0 flex items-end gap-1 rounded-tl-xl bg-white/5 py-3 px-2 text-sm tracking-wide text-white/70  duration-1000 hover:bg-black hover:text-white hover:shadow-[0_0_30px_0px_#50ffa8] lg:px-8 lg:py-4">
+        <span className="border-b border-transparent text-xs duration-500 lg:text-sm">
+          Challenge by
+        </span>
         <Link
-          className="border-b border-transparent text-base  font-bold duration-500 hover:border-[#50ffa8] group-hover:text-3xl group-hover:text-[#50ffa8]"
+          className="border-b border-transparent text-xs  font-bold duration-500 hover:border-[#50ffa8]  group-hover:text-[#50ffa8] lg:text-sm"
           target={"_blank"}
           href={"https://www.frontendmentor.io/profile/Yewdev"}
         >
           Frontend Mentor.
         </Link>
-        <span className="duration-500 group-hover:text-2xl">Coded by</span>
+        <span className="border-b border-transparent text-xs duration-500 lg:text-sm">
+          Coded by
+        </span>
         <Link
-          className="border-b border-transparent  text-base font-bold duration-500 hover:border-[#50ffa8] group-hover:text-3xl group-hover:text-[#50ffa8]"
+          className="border-b border-transparent text-xs font-bold duration-500 hover:border-[#50ffa8] group-hover:text-[#50ffa8] lg:text-sm"
           target={"_blank"}
           href={"https://github.com/Yewdev"}
         >
